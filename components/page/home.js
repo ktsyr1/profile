28448073946c182a946d39dd7e74c1df02b32ca3
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-09-12 23:14:20 +0300

    add v

--- components/page/home.js
+++ components/page/home.js
@@ -20,6 +20,7 @@ export default function Home(props) {
                     </div>
                     <iframe width="100%" height="300px" src="https://www.youtube.com/embed/WEAKF7PyypI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="ui"></iframe>
                 </section>
+            </>
         )
     }
 }
