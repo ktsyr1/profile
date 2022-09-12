3a318e393b5e00c93ae8e6162546d7b4157ab7ab
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-09-12 23:13:42 +0300

    assddd

--- components/page/home.js
+++ components/page/home.js
@@ -18,8 +18,8 @@ export default function Home(props) {
                             <p className="task" style={{ textAlgin: 'start' }}>{about}</p>
                         </div>
                     </div>
+                    <iframe width="100%" height="300px" src="https://www.youtube.com/embed/WEAKF7PyypI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="ui"></iframe>
                 </section>
-                <iframe width="100%" height="300px" src="https://www.youtube.com/embed/WEAKF7PyypI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="ui"></iframe>            </>
         )
     }
 }
