960a2f3a89b327b48de976b471ae22c29a6980f5
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2023-01-04 20:22:15 +0200

    i18

--- next.config.js
+++ next.config.js
@@ -10,19 +10,7 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
     reactStrictMode: true,
     i18n: {
-        // These are all the locales you want to support in
-        // your application
         locales: ['en', 'ar'],
-        // This is the default locale you want to be used when visiting
-        // a non-locale prefixed path e.g. `/hello`
-        defaultLocale: 'ar',
-        // This is a list of locale domains and the default locale they
-        // should handle (these are only required when setting up domain routing)
-        domains: [
-            {
-                domain: '/',
-                defaultLocale: 'ar',
-            }
-        ],
+        defaultLocale: 'ar'
     }
 })
