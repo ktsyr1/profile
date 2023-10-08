10db8d38617535f02309b7b804bc5d825a23cc2b
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2023-10-08 20:03:45 +0300

    Update project.js

--- pages/project.js
+++ pages/project.js
@@ -19,7 +19,7 @@ export default function Home(props) {
         <div className='box grid center-col' dir={info[lang]?.direction}>
             {data?.map((project, i) => {
                 let { url, title, description, year, icon, tasks, archive } = project
-                if(archive) {
+                if(!archive) {
                     return (
                     <div className={' box col w-300 ui m space '} dir={info[lang]?.direction} key={i}>
                         <div className=' box row center' >
